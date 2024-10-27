351acb977896e6242e37d446c90ffcdae4c2d18e
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-10-27 15:42:37 -0300

    Add files via upload

---
+++ Desafio 3.py
@@ -0,0 +1,6 @@
+'Crie um script python , que leia dois números e tente mostrar a soma entre eles.'
+
+primeironumero= int(input('Digite seu número:'))
+segundonumero= int(input('Digite seu número:'))
+soma=primeironumero+segundonumero
+print('A soma é:',primeironumero+segundonumero)
